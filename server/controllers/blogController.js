7b176ee7405827ed61577740d463b469dcead1cb
konepadith <konepadithspydee@gmail.com>
2023-11-08 12:59:36 +0700

    pelay comit

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -2,46 +2,76 @@
 const Blogs = require("../models/blogs")
 
 const slugify = require("slugify")
-const {v4: uuidv4 }= require("uuid")
+const { v4: uuidv4 } = require("uuid")
 
 //contact to database
-module.exports.blogs =async (req,res,next)=>{
-    try {
-        const blogs = await Blogs.find({}).exec();
-        res.json(blogs)
-      } catch (error) {
-        console.error(error);
-      }
-}
-module.exports.create=async (req,res,next)=>{
-    
-        try {
-          const {title,content,author} = req.body
-    let slug = slugify(title)
-
-    if(!slug)slug=uuidv4();
-    //validate Data
-    switch(true){
-        case !title:
-            return res.status(400).json({error:"please input title"})
-            break;
-        case !content:
-            return res.status(400).json({error:"please input content"})
-            break;   
-    }
-            const blogs = await Blogs.create({title,content,author,slug});
-            res.json(blogs)
-          } catch (error) {
-            res.status(400).json({error:"Title is already exit"})
-          } 
-}
-module.exports.singleBlog=async(req,res,next)=>{
- 
+module.exports.blogs = async (req, res, next) => {
   try {
-    const {slug} = req.params
-    const blog = await Blogs.findOne({slug}).exec()
-    res.json(blog)
+    const blogs = await Blogs.find({}).exec();
+    res.json(blogs);
   } catch (error) {
     console.error(error);
+
+    // Respond with a 500 status and an error message
+    res.status(500).json({ error: "An error occurred while fetching the documents" });
+  }
+}
+module.exports.create = async (req, res, next) => {
+
+  try {
+    const { title, content, author } = req.body;
+    let slug = slugify(title);
+
+    if (!slug) slug = uuidv4();
+
+    // Validate Data
+    switch (true) {
+      case !title:
+        return res.status(400).json({ error: "Please input a title" });
+      case !content:
+        return res.status(400).json({ error: "Please input content" });
     }
+
+    // Create a new blog document
+    const blog = await Blogs.create({ title, content, author, slug });
+    res.json(blog);
+  } catch (error) {
+    // Handle errors, such as a duplicate title
+    res.status(400).json({ error: "Title is already in use" });
+  }
+}
+module.exports.singleBlog = async (req, res, next) => {
+
+  try {
+    const { slug } = req.params;
+    // Use `await` with `findOne` to wait for the query to complete
+    const blog = await Blogs.findOne({ slug }).exec();
+
+    if (blog) {
+      // If a document is found, respond with the document as JSON
+      res.json(blog);
+    } else {
+      // If no document is found, you can handle it as needed
+      res.status(404).json({ message: "Document not found" });
+    }
+  } catch (error) {
+    // Handle any errors that may occur during the operation
+    console.error("Error:", error);
+    res.status(500).json({ message: "An error occurred while retrieving the document" });
+  }
+}
+module.exports.remove = async (req, res, next) => {
+  try {
+    const { slug } = req.params;
+    const deletedBlog = await Blogs.findOneAndDelete({ slug }).exec();
+
+    if (deletedBlog) {
+      res.json({ message: "Delete Successful" });
+    } else {
+      res.status(404).json({ message: "Document not found" });
+    }
+  } catch (error) {
+    console.error({ "DeleteError": error });
+    res.status(500).json({ message: "An error occurred while deleting the document" });
+  }
 }
--- server/routes/blog.js
+++ server/routes/blog.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router()
-const {create,blogs,singleBlog} = require("../controllers/blogController")
+const {create,blogs,singleBlog,remove} = require("../controllers/blogController")
 
 router.get('/blogs',blogs)
 router.post('/create',create)
 router.get('/blog/:slug',singleBlog)
+router.delete('/blog/:slug',remove)
 
 module.exports = router
